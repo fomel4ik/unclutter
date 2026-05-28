62be4044e57411ac67f1339e7803953b333c7244
fomel4ik <fomel4ik.yt@gmail.com>
2026-05-28 18:48:43 +0700

    chore: initial commit

---
+++ src/client/java/com/unclutter/client/UnclutterClient.java
@@ -0,0 +1,10 @@
+package com.unclutter.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UnclutterClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/unclutter/client/UnclutterDataGenerator.java
@@ -0,0 +1,11 @@
+package com.unclutter.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class UnclutterDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
